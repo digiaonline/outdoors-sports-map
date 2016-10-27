0c9dc6344b20ba2dfef797bce96b67fa5080c91b
Jonne <jonne.tanninen@gmail.com>
2016-10-27 11:41:00 +0300

    Change marker popup content

--- src/modules/unit/components/MapView.js
+++ src/modules/unit/components/MapView.js
@@ -1,16 +1,25 @@
-//@flow
+// @flow
 import React, {Component, PropTypes} from 'react';
 import {View} from './View.js';
 import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
 import {getAttr} from '../helpers.js';
+import {translate} from 'react-i18next';
 
-const UnitMarker = ({position, name, address}) =>
+const UnitMarker = translate()(({position, name, address, www, description, t}) => (
   <Marker position={position}>
     <Popup>
-      <span>{name}<br/>{address}</span>
+      <PopupContent name={name} address={address} www={www} description={description} t={t}/>
     </Popup>
-  </Marker>;
+  </Marker>));
 
+const PopupContent = ({name, address, www, description, t}) => (
+  <div className="view-popup__content">
+    <h3>{name}</h3>
+    <h4>{address}</h4>
+    <p><strong>{t('LIST.STATE')}:</strong> {t('LIST.UNKNOWN')}</p> {/*TODO: replace hard coding with real condition*/}
+    <p>{description}</p>
+    <p><a href={www} target="_blank">{t('VIEW.POPUP.OPENING_HOURS')}</a></p>
+  </div>);
 
 export class MapView extends Component {
   static propTypes = {
@@ -18,11 +27,16 @@ export class MapView extends Component {
     units: PropTypes.array
   };
 
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
     const {position, units, selected} = this.props;
     return (
       <View id="map-view" className="map-view" isSelected={selected}>
-        <Map center={position} zoom={12}>
+        <Map zoomControl={false} center={position} zoom={12} >
           <TileLayer
         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -32,8 +46,10 @@ export class MapView extends Component {
               (unit, index) =>
                 <UnitMarker
                   position={unit.location.coordinates.reverse()}
+                  www={getAttr(unit.www_url)}
                   name={getAttr(unit.name)}
                   address={getAttr(unit.street_address)}
+                  description={getAttr(unit.description)}
                   key={index} />)}
         </Map>
       </View>
