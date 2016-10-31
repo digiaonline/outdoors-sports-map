4a55b8cc0f786fa3dc56511b547919383586ae70
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-10-31 13:24:46 +0200

    Add locate me -button

--- src/modules/unit/components/MapView.js
+++ src/modules/unit/components/MapView.js
@@ -31,6 +31,7 @@ export class MapView extends Component {
     super(props);
 
     this.onMoveend = this.onMoveend.bind(this);
+    this.locateUser = this.locateUser.bind(this);
   }
 
   onMoveend(e: Object): void{
@@ -38,11 +39,15 @@ export class MapView extends Component {
     this.props.handleMoveend(center);
   }
 
+  locateUser() {
+    this.refs.map.leafletElement.locate({setView: true});
+  }
+
   render() {
     const {position, units, selected} = this.props;
     return (
       <View id="map-view" className="map-view" isSelected={selected}>
-        <Map ref="map"zoomControl={false} center={position} zoom={12} onMoveend={this.onMoveend} >
+        <Map ref="map" zoomControl={false} center={position} zoom={12} onMoveend={this.onMoveend} >
           <TileLayer
         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -58,6 +63,7 @@ export class MapView extends Component {
                   description={getAttr(unit.description)}
                   key={index} />)}
         </Map>
+        <button style={{position:'fixed',  zIndex:10000, bottom: 0, right: 0}} onClick={this.locateUser}>Locate</button>
       </View>
     );
   }
