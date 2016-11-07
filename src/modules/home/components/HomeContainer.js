9e8995cefcbf70abe37f0076988fdd7a5c19b3a4
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-11-07 15:54:17 +0200

    Add bottom logo and disclaimer

--- src/modules/home/components/HomeContainer.js
+++ src/modules/home/components/HomeContainer.js
@@ -11,8 +11,6 @@ import SingleUnitModalContainer from '../../unit/components/SingleUnitModalConta
 import {locations, views} from '../constants.js';
 import {arrayifyQueryValue} from '../../common/helpers';
 
-const Footer = ({children}) => <div>{children}</div>;
-
 export class HomeContainer extends Component {
   static propTypes = {
     fetchUnits: PropTypes.func.isRequired,
@@ -75,7 +73,6 @@ export class HomeContainer extends Component {
         <UnitBrowser units={unitData} activeFilter={activeFilter} handleClick={this.toggleModal} />
         <MapView handleMoveend={this.handleMapMove} selected={selectedView === views.MAP} position={position} units={unitData} handleClick={this.toggleModal}/>
         <SingleUnitModalContainer isOpen={this.state.modalOpen} units={unitData} params={params} handleClick={this.toggleModal} />
-        <Footer/>
       </div>
     );
   }
