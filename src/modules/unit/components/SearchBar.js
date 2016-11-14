8147dc4a7be3baea14cbc79e04b61f3ab665ded1
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-11-14 12:31:01 +0200

    Floating search suggestions

--- src/modules/unit/components/SearchBar.js
+++ src/modules/unit/components/SearchBar.js
@@ -1,30 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link, withRouter} from 'react-router';
 import {translate} from 'react-i18next';
 import {Glyphicon} from 'react-bootstrap';
 import ObservationStatus from './ObservationStatus';
 import {getAttr, getUnitIconURL, getServiceName, getObservation} from '../helpers';
 
-
-const SearchBar = translate()(({handleChange, searchResults, isLoading = true, t}) =>
-  <div>
-    <div className="search-container">
-      <label htmlFor="search"><Glyphicon glyph="search"/></label>
-      <input name="search"
-             id="search"
-             type="text"
-             onChange={(e) => handleChange(e.target.value)}
-             placeholder={isLoading ? t('GENERAL.LOADING') : `${t('SEARCH.SEARCH')}...`}
-             disabled={isLoading}/>
-    </div>
-    <SearchResults searchResults={searchResults}/>
-  </div>
-);
-
 const SearchResults = ({searchResults}) => (
   <div className="search-results">
     {searchResults.length > 0
-      ? <div>
+      ? <div className="search-results__list">
           <a>näytä kaikki tulokset</a>
           {searchResults.map((result, index) =>
             <SearchResult key={index} unit={result}/>
@@ -47,4 +31,25 @@ const SearchResult = ({unit, ...rest}) =>
   </Link>;
 
 
-export default withRouter(SearchBar);
+class SearchBar extends Component {
+  render() {
+    const {handleChange, searchResults, isLoading = true, t} = this.props;
+    return (
+      <div className="search-bar">
+        <div className="search-bar__input">
+          <label htmlFor="search"><Glyphicon glyph="search"/></label>
+          <input name="search"
+                 id="search"
+                 type="text"
+                 onChange={(e) => handleChange(e.target.value)}
+                 placeholder={isLoading ? t('GENERAL.LOADING') : `${t('SEARCH.SEARCH')}...`}
+                 disabled={isLoading}/>
+        </div>
+        <SearchResults searchResults={searchResults}/>
+      </div>
+    );
+  }
+}
+
+
+export default withRouter(translate()(SearchBar));
