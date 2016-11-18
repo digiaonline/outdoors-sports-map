938e505645faf9e31e726a12f07981cc67126c0d
Jonne <jonne.tanninen@gmail.com>
2016-11-18 15:32:43 +0200

    Refactor getAttr

--- src/modules/search/components/UnitSuggestions.js
+++ src/modules/search/components/UnitSuggestions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
 import ObservationStatus from '../../unit/components/ObservationStatus';
-import {getUnitIconURL, getServiceName, getObservation} from '../../unit/helpers';
+import {getUnitIconURL, getServiceName, getObservation, getAttr} from '../../unit/helpers';
 
 const UnitSuggestion = ({unit, ...rest}, context) =>
   <Link to={`/unit/${unit.id}`} className="search-suggestions__result" {...rest}>
@@ -9,13 +9,13 @@ const UnitSuggestion = ({unit, ...rest}, context) =>
       <img src={getUnitIconURL(unit)} alt={getServiceName(unit)} />
     </div>
     <div className="search-suggestions__result-details">
-      <div className="search-suggestions__result-details__name">{context.getAttr(unit.name)}</div>
+      <div className="search-suggestions__result-details__name">{getAttr(unit.name, context.getActiveLanguage())}</div>
       <ObservationStatus observation={getObservation(unit)}/>
     </div>
   </Link>;
 
 UnitSuggestion.contextTypes = {
-  getAttr: React.PropTypes.func
+  getActiveLanguage: React.PropTypes.func
 };
 
 export default UnitSuggestion;
