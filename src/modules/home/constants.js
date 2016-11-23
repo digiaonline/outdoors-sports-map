2009717723112719dc67de694f3db9a5a8bf1d4a
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-11-23 10:21:52 +0200

    Change app name to “Ulkoliikuntakartta”

--- src/modules/home/constants.js
+++ src/modules/home/constants.js
@@ -1,9 +1,5 @@
 import {normalizeActionName} from '../common/helpers';
 
-export const homeActions = {
-  SET_HELLO_MESSAGE: normalizeActionName('home/SET_HELLO_MESSAGE')
-};
-
 export const locations = {
   HELSINKI: [60.171944, 24.941389],
   ESPOO: [60.19792, 24.708885],
