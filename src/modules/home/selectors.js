c0391b6f66b951f0158db116acee0798f86b956f
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-12-08 10:10:57 +0200

    Fix isLoading selector bug

--- src/modules/home/selectors.js
+++ src/modules/home/selectors.js
@@ -2,5 +2,5 @@
 import isEmpty from 'lodash/isEmpty';
 
 export const getIsLoading = (state: AppState) =>
-  state.unit.isFetching && state.service.isFetching
-  && isEmpty(state.unit) && isEmpty(state.service);
+  state.unit.isFetching && isEmpty(state.unit.all) ||
+  state.service.isFetching && isEmpty(state.service.all);
