c18147080cca884fced07fb4f2610cd6ff8f6b65
Timo Tuominen <tituomin@gmail.com>
2016-12-27 18:04:26 +0200

    Formatting: fix indentation.

--- src/modules/unit/components/UnitGeometry.js
+++ src/modules/unit/components/UnitGeometry.js
@@ -10,33 +10,35 @@ class UnitGeometry extends React.Component {
   }
   render () {
     const {unit, isSelected, ...rest} = this.props;
-    return (<div className="unit-geometry">
-    { // hilight background for selected unit
-      isSelected &&
-      <GeoJSON
-        className={
-          `unit-geometry__hilight ${isSelected ? 'unit-geometry__hilight--show' : ''}`
+    return (
+      <div className="unit-geometry">
+        { // hilight background for selected unit
+          isSelected &&
+          <GeoJSON
+            className={
+              `unit-geometry__hilight ${isSelected ? 'unit-geometry__hilight--show' : ''}`
+            }
+            key={`${unit.id}_hilight`}
+            opacity={isSelected ? 1 : 0}
+            data={unit.geometry}
+            {...rest}
+           />
         }
-        key={`${unit.id}_hilight`}
-        opacity={isSelected ? 1 : 0}
-        data={unit.geometry}
-        {...rest}
-       />
-    }
-    <GeoJSON // Click area
-      className="unit-geometry__click-area"
-      key={`${unit.id}_click-area`}
-      data={unit.geometry}
-      {...rest} />
-    <GeoJSON // actual track
-      ref='feature'
-      className={
-        `unit-geometry__track unit-geometry__track--${getUnitQuality(unit)}`
-      }
-      key={unit.id}
-      data={unit.geometry}
-      {...rest} />
-  </div>);
+        <GeoJSON // Click area
+          className="unit-geometry__click-area"
+          key={`${unit.id}_click-area`}
+          data={unit.geometry}
+          {...rest} />
+        <GeoJSON // actual track
+          ref='feature'
+          className={
+            `unit-geometry__track unit-geometry__track--${getUnitQuality(unit)}`
+          }
+          key={unit.id}
+          data={unit.geometry}
+          {...rest} />
+      </div>
+    );
   }
 }
 
