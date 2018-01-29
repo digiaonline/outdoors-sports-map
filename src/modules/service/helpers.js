fcc2edd40fb20d969e9ec3c983254cef70bec768
Ville Vaarala <vaaralav@users.noreply.github.com>
2018-01-29 15:35:37 +0200

    Add support for units with multiple services (#57)
    Fixes #56

--- src/modules/service/helpers.js
+++ src/modules/service/helpers.js
@@ -3,9 +3,15 @@ import {DEFAULT_LANG} from '../common/constants';
 import {getAttr} from '../unit/helpers';
 import isEmpty from 'lodash/isEmpty';
 
-export const getServiceName = (id: number, services: Array<Object>, language: ?string = DEFAULT_LANG) => {
+export const getServiceName = (unitServices: number[], services: Object, language: ?string = DEFAULT_LANG) => {
   if (!services || isEmpty(services)) {
     return '';
   }
-  return getAttr(services[id].name, language);
+  for (const id of unitServices) {
+    const service = services[id];
+    if (service && typeof service.name !== 'undefined') {
+      return getAttr(services[id].name, language);
+    }
+  }
+  return '';
 };
