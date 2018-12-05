57267efae2ad0cc746486307aa131d37f1f79567
Hannes Alarotu <hannes.alarotu@codento.com>
2018-12-05 14:44:06 +0200

    fix api version number to constants

--- src/modules/common/constants.js
+++ src/modules/common/constants.js
@@ -3,7 +3,7 @@ import type {UnitState} from '../unit/constants';
 import type {ServiceState} from '../service/constants';
 import type {SearchState} from '../search/constants';
 
-export const API_URL = 'https://api.hel.fi/servicemap/v2'; // 'http://servicemat.hel.ninja/servicemap/v1';
+export const API_URL = 'https://api.hel.fi/servicemap/v1'; // 'http://servicemat.hel.ninja/servicemap/v1';
 export const DIGITRANSIT_API_URL = 'https://api.digitransit.fi/geocoding/v1';
 export const APP_NAME = 'outdoors-sports-map';
 
