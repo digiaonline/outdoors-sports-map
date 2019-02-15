3e957a8c051c1c4d068060e0848c928fd30f1760
Sam Stenvall <sam.stenvall@nordsoftware.com>
2019-02-15 11:50:06 +0200

    Update the Capistrano configuration to work with the latest version
    The configuration is now inline since the ops repository is gone. The key should be placed in cap/keys instead.

---
+++ cap/deploy/production.rb
@@ -0,0 +1,7 @@
+server 'ulkoliikunta.fi', user: 'outdoors-sports-map', roles: %w{ui}, name: "outdoors-sports-map-production"
+
+set :ssh_options, {
+  keys: %w(../keys/outdoors-sports-map),
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
