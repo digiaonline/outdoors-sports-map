ea77269b4ac9c5bcbe375b6609a36d8b2bbd0198
Ville Koivunen <ville.koivunen@hel.fi>
2019-06-07 15:37:52 +0300

    Use https for loading the tiles

--- src/modules/map/constants.js
+++ src/modules/map/constants.js
@@ -1,6 +1,6 @@
 import {normalizeActionName} from '../common/helpers.js';
 
-export const MAP_URL = 'http://tiles.hel.ninja/styles/hel-osm-light/{z}/{x}/{y}.png';
+export const MAP_URL = 'https://tiles.hel.ninja/styles/hel-osm-light/{z}/{x}/{y}.png';
 export const MAP_RETINA_URL = MAP_URL.replace('.png','@2x.png');
 
 export const DEFAULT_ZOOM = 12;
